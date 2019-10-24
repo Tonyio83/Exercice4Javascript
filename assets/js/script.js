90a382e8c282e5243a59b60b3da202b2608605d0
Tonyio83 <anthonyfrancelle@gmail.com>
2019-10-24 09:57:12 +0200

    exercice 4 fini

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function validate(){
+  var lastname = document.getElementById('lastname').value;
+  var firstname = document.getElementById('firstname').value;
+  var city = document.getElementById('city').value;
+  alert("Nom : " + lastname + "\nPrénom : " + firstname + "\nVille : " + city);
+}
